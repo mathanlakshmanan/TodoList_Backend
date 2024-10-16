77dc4c8a4988e2c81fb9007d41df0588bd231b45
mathanlakshmanan <mathankumarmca92@gmail.com>
2024-10-17 02:54:15 +0530

    cors added

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv').config();
 const app = express();
 app.use(express.json());
+
+app.use(cors());
 
 mongoose.connect(process.env.DBURL).then(()=>{
     console.log("DB Conneted");
